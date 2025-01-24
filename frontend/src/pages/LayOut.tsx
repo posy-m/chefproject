c0391c461f6106cc86f1fee57fe9e8950113f704
posy-m <minji.posy@gmail.com>
2025-01-24 18:47:02 +0900

    [frontend] layout

--- frontend/src/pages/LayOut.tsx
+++ frontend/src/pages/LayOut.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import styles from '../styles/Layout.module.css'
 
 const LayOut = () => {
   return (
     <div>
       <Header />
-      <main>
+      <main className={styles['layout-main']}>
         <Outlet />
       </main>
 
