6b8b7a5b43ef35573960aa4b4149fcb32a27a7b7
posy-m <minji.posy@gmail.com>
2025-01-31 22:39:24 +0900

    [frontend]usersignin password style

--- frontend/src/pages/UserSignUpPage.tsx
+++ frontend/src/pages/UserSignUpPage.tsx
@@ -7,17 +7,13 @@ const UserSignUpPage = () => {
   const [showPw, setShowPw] = useState<boolean>(false);
   const [showConfirmPw, setShowConfirmPw] = useState<boolean>(false);
 
-  const handlePasswordType = () => {
-    setShowPw((prev) => !prev);
-  };
-
   return (
     <div className={styles['signup-box']}>
       <span>개입 회원가입</span>
       <div className={styles['signup-wrap']}>
         <input placeholder='이름' className={styles['default-input']} required />
         <div className={styles['signup-doublecheck']}>
-          <input placeholder='닉네임' required />
+          <input placeholder='닉네임 6글자이내' maxLength={6} required />
           <button>중복확인</button>
         </div>
         <div className={styles['signup-doublecheck']}>
@@ -28,14 +24,14 @@ const UserSignUpPage = () => {
           <input placeholder='휴대폰번호' required />
           <button>중복확인</button>
         </div>
-        <div>
+        <div className={styles['password-container']}>
           <input placeholder='비밀번호(특수기호 포함 5글자~20글자)' className={styles['default-input']} type={showPw ? 'text' : 'password'} required minLength={5} maxLength={20} />
-          <span onClick={() => setShowPw((prev) => !prev)}>{showPw ? <AiFillEye /> : <AiFillEyeInvisible />}</span>
+          <span onClick={() => setShowPw((prev) => !prev)}>{showPw ? <AiFillEye size={25} /> : <AiFillEyeInvisible size={25} />}</span>
         </div>
-        <div>
+        <div className={styles['password-container']}>
           <input placeholder='비밀번호 확인' className={styles['default-input']} type={showConfirmPw ? 'text' : 'password'} required minLength={5} maxLength={20} />
           <span onClick={() => setShowConfirmPw((prev) => !prev)}>
-            {showConfirmPw ? <AiFillEye /> : <AiFillEyeInvisible />}</span>
+            {showConfirmPw ? <AiFillEye size={25} /> : <AiFillEyeInvisible size={25} />}</span>
         </div>
 
 
