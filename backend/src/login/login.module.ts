b605bd123f34d732f9a2004d32475d15da128166
posy-m <minji.posy@gmail.com>
2025-02-03 01:41:22 +0900

    [backend] login service

--- backend/src/login/login.module.ts
+++ backend/src/login/login.module.ts
@@ -5,9 +5,12 @@ import { Users } from 'src/models/userDB.entity';
 import { CompanyInfo } from 'src/models/companyInfo.entity';
 import { ManagerSite } from 'src/models/managerSite.entity';
 import { LoginController } from './login.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Users, CompanyInfo, ManagerSite])],
+  imports: [SequelizeModule.forFeature([Users, CompanyInfo, ManagerSite])
+    , JwtModule.register({ signOptions: { expiresIn: '60s' } }), ConfigModule.forRoot()],
   controllers: [LoginController],
   providers: [LoginService]
 })
--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -4,13 +4,31 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Users, UserType } from 'src/models/userDB.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from './dto/user.dto';
+
 
 @Injectable()
 export class LoginService {
   constructor(
     @InjectModel(Users)
-    private userModel: typeof Users,
+    private readonly userModel: typeof Users,
+    private readonly jwtService: JwtService
   ) { }
+
+  // 회원가입
+  async create(createUser: CreateUserDto): Promise<Users> {
+    const { name, email, phoneNumber, password } = createUser
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(createUser.password, salt)
+    console.log(hashedPassword, "이거 ");
+    return this.userModel.create({
+      name, email, phoneNumber, password: hashedPassword
+    })
+  }
+
+  //로그인
 
 
   // 관리자 아이디 생성
