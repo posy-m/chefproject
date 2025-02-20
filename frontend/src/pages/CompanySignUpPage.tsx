6eba2fdb033b1454a53428efb3f1b2d861130637
posy-m <minji.posy@gmail.com>
2025-02-21 00:19:11 +0900

    [frontend] companysignup show password

--- frontend/src/pages/CompanySignUpPage.tsx
+++ frontend/src/pages/CompanySignUpPage.tsx
@@ -1,8 +1,14 @@
 
 import styles from '../styles/UserSignupPage.module.css'
-import React from 'react'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import React, { useState } from 'react'
 
 const CompanySignUpPage = () => {
+  const [password, setPassword] = useState<string>('')
+  const [showPw, setShowPw] = useState<boolean>(false);
+
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+  const [showConfirmPw, setShowConfirmPw] = useState<boolean>(false);
   return (
     <div className={styles['signup-box']}>
       <span>기업 회원가입</span>
@@ -20,8 +26,19 @@ const CompanySignUpPage = () => {
           <input placeholder='휴대폰번호' className='' />
           <button>중복확인</button>
         </div>
-        <input placeholder='비밀번호(특수기호 포함 5글자~20글자)' className={styles['default-input']} type='password' />
-        <input placeholder='비밀번호 확인' className={styles['default-input']} type='password' />
+        <div className={styles['password-container']}>
+          <input placeholder='비밀번호(특수기호 포함 5글자~20글자)' className={styles['default-input']}
+            type={showPw ? 'text' : 'password'} required minLength={5} maxLength={20}
+            onChange={(e) => setPassword(e.target.value)} value={password} />
+          <span onClick={() => setShowPw((prev) => !prev)}>{showPw ? <AiFillEye size={25} /> : <AiFillEyeInvisible size={25} />}</span>
+        </div>
+        <div className={styles['password-container']}>
+          <input placeholder='비밀번호 확인' className={styles['default-input']}
+            type={showConfirmPw ? 'text' : 'password'} required minLength={5} maxLength={20}
+            onChange={(e) => setConfirmPassword(e.target.value)} />
+          <span onClick={() => setShowConfirmPw((prev) => !prev)}>{showConfirmPw ? <AiFillEye size={25} /> : <AiFillEyeInvisible size={25} />}</span>
+
+        </div>
       </div>
       <button>회원가입</button>
     </div>
